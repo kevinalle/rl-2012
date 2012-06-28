16751bad6fa1c47920e61ac9b03088f14a729d76
kevinalle <kevinalle@5eba630a-98d1-509a-3ca0-3178cf03187c>
2012-06-28 20:49:01 +0000

    LINT

--- cube-solver/src/cubesolver/astar.py
+++ cube-solver/src/cubesolver/astar.py
@@ -1,165 +1,193 @@
 #!/usr/bin/env python
+"""Solve Cube with A*"""
 
-import sys, re, random, heapq
+import sys, re, heapq
 
-def ls2int(l):
-  """Codifica una lista de Bools en un entero"""
-  return int(''.join([e and "1" or "0" for e in l]), 2)
+
+def ls2int(lista):
+    """Codifica una lista de Bools en un entero"""
+    return int(''.join([e and "1" or "0" for e in lista]), 2)
 
 def int2ls(i, length):
-  """Decodifica un int en una lista de Bools con longitud length (su repr binaria)"""
-  return [b=="1" for b in (bin(i)[2:]).zfill(length)]
+    """Decodifica un int en una lista de Bools con longitud length
+    (su repr binaria)"""
+    return [b == "1" for b in (bin(i)[2:]).zfill(length)]
 
-def xy2i(x, y, w):
-  """Pasa de coordenadas de una matriz a un indice de su representacion con vector unidimensional"""
-  return y*w+x
+def xy2i(xpos, ypos, width):
+    """Pasa de coordenadas de una matriz a un indice de su representacion con
+    vector unidimensional"""
+    return ypos*width+xpos
 
-def i2xy(i, w):
-  """Decodifica un indice en coordenadas"""
-  return (i%w, i/w)
+def i2xy(i, width):
+    """Decodifica un indice en coordenadas"""
+    return (i%width, i/width)
 
-def pack_state(state, w):
-  """Codifica un estado en una tupla de 3 coordenadas (posicion del cubo, colores del tablero, colores del cubo)"""
-  return (xy2i(state["x"], state["y"], w), ls2int(state["board"]), ls2int(state["cube"]))
+def pack_state(state, width):
+    """Codifica un estado en una tupla de 3 coordenadas (posicion del cubo,
+    colores del tablero, colores del cubo)"""
+    return (xy2i(state["x"], state["y"], width),
+            ls2int(state["board"]),
+            ls2int(state["cube"]))
 
-def unpack_state(pstate, w, h):
-  """Decodifica una tupla en un estado (x,y del cubo, lista de Bool de los colores del tablero, lista de Bool de los colores del cubo)"""
-  i, board, cube = pstate
-  return {"x": i%w, "y": i/w, "board": int2ls(board, w*h), "cube": int2ls(cube, 6)}
+def unpack_state(pstate, width, height):
+    """Decodifica una tupla en un estado (x,y del cubo, lista de Bool de los
+    colores del tablero, lista de Bool de los colores del cubo)"""
+    i, board, cube = pstate
+    return {"x": i % width, "y": i / width,
+            "board": int2ls(board, width * height),
+            "cube": int2ls(cube, 6)}
 
 def decode(gameid):
-  """
-  Decodifica un gameID en el estado inicial y tamanio del tablero
-  
-  El gameID es un string tipo "c4x4:62A4,11", donde "c" significa que es un
-  cubo, "4x4" es el tamanio del tablero, "62A4" es la representacion hexadecimal
-  del color de las celdas del tablero y "11" es la posicion inicial del cubo.
-  """
-  if not re.match("c\d{1,2}x\d{1,2}:[A-F0-9]+,\d+", gameid):
-    print "Bad GameID"
-    exit(1)
-  w, h = map(int, gameid[1:].split(":")[0].split("x"))
-  boardid, pos = gameid.split(":")[1].split(",")
-  # construir una lista de Bool a partir de la representacin binaria del tablero
-  # (pasar el hexadecimal a binario, sacarle el "0b" del principio, volver a
-  # agregarle los 0s de la izquierda que corresponden, y truncarlo al tamanio
-  # del tablero (si no tenia multiplo de 4 celdas))
-  boardbool = (bin(int(boardid, 16))[2:]).zfill(4*len(boardid))[:w*h]
-  board = [b=="1" for b in boardbool]
-  x, y = i2xy(int(pos), w)
-  return ({"x": x, "y": y, "board": board, "cube": [False]*6}, w, h)
+    """
+    Decodifica un gameID en el estado inicial y tamanio del tablero
+    
+    El gameID es un string tipo "c4x4:62A4,11", donde "c" significa que es un
+    cubo, "4x4" es el tamanio del tablero, 62A4 es la representacion hexadecimal
+    del color de las celdas del tablero y "11" es la posicion inicial del cubo.
+    """
+    if not re.match("c\d{1,2}x\d{1,2}:[A-F0-9]+,\d+", gameid):
+        print "Bad GameID"
+        exit(1)
+    width, height = [int(val) for val in gameid[1:].split(":")[0].split("x")]
+    boardid, pos = gameid.split(":")[1].split(",")
+    # construir una lista de Bool a partir de la representacin bin del tablero
+    # (pasar el hexadecimal a binario, sacarle el "0b" del principio, volver a
+    # agregarle los 0s de la izquierda que corresponden, y truncarlo al tamanio
+    # del tablero (si no tenia multiplo de 4 celdas))
+    boardb = (bin(int(boardid, 16))[2:]).zfill(4*len(boardid))[:width * height]
+    board = [b == "1" for b in boardb]
+    xpos, ypos = i2xy(int(pos), width)
+    return ({"x": xpos, "y": ypos, "board": board,
+             "cube": [False] * 6}, width, height)
 
 # CONSTs
-N, S, W, E, B, T = 0,1,2,3,4,5
-movements = ["Up", "Down", "Left", "Right"]
+N, S, W, E, B, T = 0, 1, 2, 3, 4, 5
+MOVS = ["Up", "Down", "Left", "Right"]
 
 # El cubo se representa con una lista de Bool que representa los colores de las
 # caras: [N, S, W, E, B, T]
 # Las rotaciones devuelven el estado del cubo despues de haber rotado en esa
 # direccion
-def rot_N(cube):
-  return [cube[T], cube[B], cube[W], cube[E], cube[N], cube[S]]
+def rot_north(cube):
+    """Return ratated cube list"""
+    return [cube[T], cube[B], cube[W], cube[E], cube[N], cube[S]]
 
-def rot_S(cube):
-  return [cube[B], cube[T], cube[W], cube[E], cube[S], cube[N]]
+def rot_south(cube):
+    """Return ratated cube list"""
+    return [cube[B], cube[T], cube[W], cube[E], cube[S], cube[N]]
 
-def rot_W(cube):
-  return [cube[N], cube[S], cube[T], cube[B], cube[W], cube[E]]
+def rot_west(cube):
+    """Return ratated cube list"""
+    return [cube[N], cube[S], cube[T], cube[B], cube[W], cube[E]]
 
-def rot_E(cube):
-  return [cube[N], cube[S], cube[B], cube[T], cube[E], cube[W]]
+def rot_east(cube):
+    """Return ratated cube list"""
+    return [cube[N], cube[S], cube[B], cube[T], cube[E], cube[W]]
 
-def rot(cube, to):
-  """Llama a la rotacion correspondiente segun el int to"""
-  return [rot_N, rot_S, rot_W, rot_E][to](cube)
+def rot(cube, rot_to):
+    """Llama a la rotacion correspondiente segun el int to"""
+    return [rot_north, rot_south, rot_west, rot_east][rot_to](cube)
 
 def iswinp(pstate):
-  """Un estado es WIN si el cubo esta todo pintado, osea si el estado del cubo
-  es [True,True,True,True,True,True], osea su representacion en int es 63"""
-  return pstate[2] == 63
+    """Un estado es WIN si el cubo esta todo pintado, osea si el estado del cubo
+    es [True,True,True,True,True,True], osea su representacion en int es 63"""
+    return pstate[2] == 63
 
-def draw(state, w, h):
-  board = [b and "#" or "_" for b in state["board"]]
-  board[state["y"]*w+state["x"]] = "C"
-  board_str = '\n'.join([''.join(board[i*w:i*w+w]) for i in range(h)])
-  print board_str
+def draw(state, width, height):
+    """Representacion ASCII del estado"""
+    board = [b and "#" or "_" for b in state["board"]]
+    board[state["y"] * width + state["x"]] = "C"
+    board_str = '\n'.join(
+        [''.join(board[i * width:(i + 1) * width]) for i in range(height)])
+    print board_str
 
-def can(state, w, h):
-  """Devuelve las acciones que puede tomar desde el estado actual"""
-  return [i for (i,c) in enumerate([state["y"]>0, state["y"]<h-1, state["x"]>0, state["x"]<w-1]) if c]
+def can(state, width, height):
+    """Devuelve las acciones que puede tomar desde el estado actual"""
+    bounds = [state["y"] > 0, state["y"] < height - 1,
+            state["x"] > 0, state["x"] < width - 1]
+    return [i for (i, c) in enumerate(bounds) if c]
 
-def go(state, to, w):
-  """Devuelve el estado resultante de tomar la accion to desde state"""
-  board, cube = state["board"][:], rot(state["cube"], to)
-  x, y = [lambda x,y: (x,y-1), lambda x,y: (x,y+1), lambda x,y: (x-1,y), lambda x,y: (x+1,y)][to](state["x"], state["y"])
-  board[y*w+x], cube[B] = cube[B], board[y*w+x]
-  return {"x": x, "y": y, "board": board, "cube": cube}
+def moveto(state, rot_to, width):
+    """Devuelve el estado resultante de tomar la accion to desde state"""
+    board = state["board"][:]
+    # Rotar el cubo
+    cube = rot(state["cube"], rot_to)
+    # Actualizar x, y
+    actions = [lambda x, y: (x, y - 1), lambda x, y: (x, y + 1),
+               lambda x, y: (x - 1, y), lambda x, y: (x + 1, y)]
+    xpos, ypos = actions[rot_to](state["x"], state["y"])
+    # Swap de los colores cubo-tablero
+    board[ypos * width + xpos], cube[B] = cube[B], board[ypos * width + xpos]
+    return {"x": xpos, "y": ypos, "board": board, "cube": cube}
 
-def vecinos(pstate, w, h):
-  """Devuelve los estados vecinos del estado actual (empacados)"""
-  state = unpack_state(pstate, w, h)
-  return [(to, pack_state(go(state, to, w), w)) for to in can(state, w, h)]
+def vecinos(pstate, width, height):
+    """Devuelve los estados vecinos del estado actual (empacados)"""
+    state = unpack_state(pstate, width, height)
+    return [(to, pack_state(moveto(state, to, width), width))
+                for to in can(state, width, height)]
 
-def solve(pstate, w, h):
-  """Busca camino minimo en el grafo de estados con A*, copiado casi textual del
-  pseudocodigo de http://en.wikipedia.org/wiki/A*_search_algorithm"""
+def solve(pstate, width, height):
+    """Busca camino minimo en el grafo de estados con A*, copiado casi textual
+    del pseudocodigo de http://en.wikipedia.org/wiki/A*_search_algorithm"""
 
-  def heuristic(pstate):
-    # Heuristica bastante relajada. Cantidad de caras que faltan levantar
-    # (claramente necesita al menos esa cantidad de pasos para ganar)
-    return 6 - bin(pstate[2]).count("1")
+    def heuristic(pstate):
+        """Heuristica bastante relajada. Cantidad de caras que faltan levantar
+        (claramente necesita al menos esa cantidad de pasos para ganar)"""
+        return 6 - bin(pstate[2]).count("1")
 
-  g = {pstate: 0}
-  f = {pstate: heuristic(pstate)}
-  # heap de los nodos a explorar
-  frontera = [(f[pstate], pstate)]
-  # conjunto de visitados, para evitar ciclos
-  visitados = set()
-  came_from = {}
-  # Reconstruccion recursiva del camino despues de llegar al goal
+    g_score = {pstate: 0}
+    f_score = {pstate: heuristic(pstate)}
+    # heap de los nodos a explorar
+    frontera = [(f_score[pstate], pstate)]
+    # conjunto de visitados, para evitar ciclos
+    visitados = set()
+    came_from = {}
 
-  def reconstruct(pstate):
-    if pstate in came_from:
-      parent, action = came_from[pstate]
-      path = reconstruct(parent)
-      return path + [action]
-    else:
-      return []
+    def reconstruct(pstate):
+        """Reconstruccion recursiva del camino despues de llegar al goal"""
+        if pstate in came_from:
+            parent, action = came_from[pstate]
+            path = reconstruct(parent)
+            return path + [action]
+        else:
+            return []
 
-  # sacar de a uno de la frontera, actualizar valor, agregar los vecinos
-  while frontera:
-    fval, ps = heapq.heappop(frontera)
-    if iswinp(ps):
-      return reconstruct(ps)
-    visitados.add(ps)
-    for action, vecino in vecinos(ps, w, h):
-      if vecino in visitados: continue
-      tentative_g = g[ps] + 1
-      if vecino not in frontera or tentative_g < g[vecino]:
-        came_from[vecino] = (ps, action)
-        g[vecino] = tentative_g
-        f[vecino] = tentative_g + heuristic(vecino)
-        heapq.heappush(frontera, (f[vecino], vecino))
-  raise Exception, "No tiene solucion :("
+    # sacar de a uno de la frontera, actualizar valor, agregar los vecinos
+    while frontera:
+        _, state = heapq.heappop(frontera)
+        if iswinp(state):
+            return reconstruct(state)
+        visitados.add(state)
+        for action, vecino in vecinos(state, width, height):
+            if vecino in visitados:
+                continue
+            tentative_g = g_score[state] + 1
+            if vecino not in frontera or tentative_g < g_score[vecino]:
+                came_from[vecino] = (state, action)
+                g_score[vecino] = tentative_g
+                f_score[vecino] = tentative_g + heuristic(vecino)
+                heapq.heappush(frontera, (f_score[vecino], vecino))
+    raise Exception, "No tiene solucion :("
 
 def main(gameid):
-  state, w, h = decode(gameid)
-  pstate = pack_state(state, w)
-  solution = solve(pstate, w, h)
-  print '\n'.join([movements[a] for a in solution])
+    """Decode GameID, find solution, print solution"""
+    state, width, height = decode(gameid)
+    pstate = pack_state(state, width)
+    solution = solve(pstate, width, height)
+    print '\n'.join([MOVS[a] for a in solution])
 
-  # Para debug, imprimo los pasos y acciones
-  #~draw(state, w, h)
-  #~for a in solution:
-    #~print movements[a]
-    #~state = go(state, a, w)
+    # Para debug, imprimo los pasos y acciones
     #~draw(state, w, h)
-    #~print state["cube"]
-    #~print state["board"]
-    #~print
+    #~for a in solution:
+        #~print MOVS[a]
+        #~state = mvoeto(state, a, w)
+        #~draw(state, w, h)
+        #~print state["cube"]
+        #~print state["board"]
+        #~print
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    main(sys.argv[1])
-  else:
-    print "Need GameId."
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print "Need GameId."
--- cube-solver/src/cubesolver/cube.py
+++ cube-solver/src/cubesolver/cube.py
@@ -1,82 +1,95 @@
 #!/usr/bin/env python
+"""Correr el cube y mandarle acciones"""
 
-import commands, os, sys, time, random, re, argparse
+import commands, os, time, random, re, argparse
 
 def move(where):
-  """Espera .1s y apreta la tecla "where" (en la ventana activa)"""
-  time.sleep(.1)
-  os.system("xdotool key %s"%where)
+    """Espera .1s y apreta la tecla "where" (en la ventana activa)"""
+    time.sleep(.1)
+    os.system("xdotool key %s"%where)
 
 def decode(gameid):
-  """
-  Extrae las condiciones iniciales del tablero a partir de un gameID
-  
-  El gameID es un string tipo "c4x4:62A4,11", donde "c" significa que es un
-  cubo, "4x4" es el tamanio del tablero, "62A4" es la representacion hexadecimal
-  del color de las celdas del tablero y "11" es la posicion inicial del cubo.
-  """
-  if not re.match("c\d{1,2}x\d{1,2}:[A-F0-9]+,\d+", gameid):
-    print "Bad GameID"
-    exit(1)
-  w, h = map(int, gameid[1:].split(":")[0].split("x"))
-  boardid, pos = gameid.split(":")[1].split(",")
-  # construir una lista a partir de la representacin binaria del tablero inicial
-  # (pasar el hexadecimal a binario, sacarle el "0b" del principio, volver a
-  # agregarle los 0s de la izquierda que corresponden, y truncarlo al tamanio
-  # del tablero (si no tenia multiplo de 4 celdas))
-  boardbool = (bin(int(boardid, 16))[2:]).zfill(4*len(boardid))[:w*h]
-  # representacion en ASCII del tablero: _ celda blanca, # celda azul, C cubo
-  board = [{"0":"_", "1": "#"}[b] for b in boardbool]
-  board[int(pos)] = "C"
-  # agregar "\n" cada w celdas
-  board_str = '\n'.join([''.join(board[i*w:i*w+w]) for i in range(h)])
-  return {"w": w, "h": h, "board": board_str, "pos": pos}
+    """
+    Extrae las condiciones iniciales del tablero a partir de un gameID
+    
+    El gameID es un string tipo "c4x4:62A4,11", donde "c" significa que es un
+    cubo, "4x4" es el tamanio del tablero, "62A4" es la representacion hexa
+    del color de las celdas del tablero y "11" es la posicion inicial del cubo.
+    """
+    if not re.match("c\d{1,2}x\d{1,2}:[A-F0-9]+,\d+", gameid):
+        print "Bad GameID"
+        exit(1)
+    width, height = [int(val) for val in gameid[1:].split(":")[0].split("x")]
+    boardid, pos = gameid.split(":")[1].split(",")
+    # construir una lista a partir de la representacin bin del tablero inicial
+    # (pasar el hexadecimal a binario, sacarle el "0b" del principio, volver a
+    # agregarle los 0s de la izquierda que corresponden, y truncarlo al tamanio
+    # del tablero (si no tenia multiplo de 4 celdas))
+    boardb = (bin(int(boardid, 16))[2:]).zfill(4*len(boardid))[:width * height]
+    # representacion en ASCII del tablero: _ celda blanca, # celda azul, C cubo
+    board = [{"0":"_", "1": "#"}[b] for b in boardb]
+    board[int(pos)] = "C"
+    # agregar "\n" cada w celdas
+    board_str = '\n'.join(
+        [''.join(board[i * width:(i + 1) * width]) for i in range(height)])
+    return {"w": width, "h": height, "board": board_str, "pos": pos}
 
-def main(game_id=None, solver=None, size=(4,4)):
-  # correr el cube con --generate, que nos genera un identificador de una
-  # instancia del juego (del tipo "c4x4:62A4,11").
-  gameid = game_id or commands.getoutput("cube --generate 1 c%dx%d"%tuple(size))
-  print gameid
-  # parsear el id del juego y extraer el tablero inicial y posicion del cubo.
-  game = decode(gameid)
-  print game["board"]
+def main(game_id=None, solver=None, size=(4, 4)):
+    """Correr cube, correr solver, mandarle las acciones del solver"""
+    # correr el cube con --generate, que nos genera un identificador de una
+    # instancia del juego (del tipo "c4x4:62A4,11").
+    gameid = game_id or commands.getoutput(
+                            "cube --generate 1 c%dx%d" % tuple(size))
+    print gameid
+    # parsear el id del juego y extraer el tablero inicial y posicion del cubo.
+    game = decode(gameid)
+    print game["board"]
 
-  if not game_id:
-    # correr el cube con la instancia que generamos antes (abre una ventana con el
-    # mismo juego que generamos y decodificamos antes.
-    os.system("cube %s &"%gameid)
-    # (darle 2s para que se abra)
-    wait2sFromNow = time.time()
+    if not game_id:
+        # correr el cube con la instancia que generamos antes (abre una ventana
+        # con el mismo juego que generamos y decodificamos antes.
+        os.system("cube %s &"%gameid)
+        # tomar el tiempo
+        starttime = time.time()
 
-  if solver:
-    output = commands.getoutput("%s %s"%(solver,gameid))
-    actions = output.strip().split("\n")
-  else:
-    moves = ["Up", "Right", "Down", "Left"]
-    actions = [random.choice(moves) for _ in xrange(20)]
-
-  # encontrar el Window ID de la ventana que abrio.
-  windowid = commands.getoutput('xwininfo -name "Cube" | grep "Window id" | cut -d\  -f4')
-  # activar esa ventana para poder mandarle teclas
-  os.system("xdotool windowactivate %s"%windowid)
-  
-  if not game_id:
-    # (darle 2s para que se abra)
-    tardo = time.time()-wait2sFromNow
-    if solver: print "solver took %ss"%tardo
-    if tardo < 2: time.sleep(2-tardo)
-
-  for a in actions:
-    if a in ["Up", "Right", "Down", "Left"]:
-      move(a)
+    if solver:
+        output = commands.getoutput("%s %s" % (solver, gameid))
+        actions = output.strip().split("\n")
     else:
-      raise Exception, "Accion invalida"
+        moves = ["Up", "Right", "Down", "Left"]
+        actions = [random.choice(moves) for _ in xrange(20)]
+
+    # encontrar el Window ID de la ventana que abrio.
+    windowid = commands.getoutput(
+                   'xwininfo -name "Cube" | grep "Window id" | cut -d\    -f4')
+    # activar esa ventana para poder mandarle teclas
+    os.system("xdotool windowactivate %s"%windowid)
+    
+    if not game_id:
+        # (darle 2s para que se abra)
+        tardo = time.time()-starttime
+        if solver:
+            print "solver took %ss" % tardo
+        if tardo < 2:
+            time.sleep(2 - tardo)
+
+    for action in actions:
+        if action in ["Up", "Right", "Down", "Left"]:
+            move(action)
+        else:
+            raise Exception, "Accion invalida"
+
+def parseargs():
+    """Parse command line arguments and run main"""
+    parser = argparse.ArgumentParser(description='Solve "Cube".')
+    parser.add_argument('--solver', default="./astar.py",
+                        help='executable that solves the algorithm')
+    parser.add_argument('--game', default=None, help='Game ID')
+    parser.add_argument('--size', nargs=2, type=int, default=(4, 4),
+                        help='Board size')
+    args = parser.parse_args()
+
+    main(game_id=args.game, solver=args.solver, size=args.size)
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Solve "Cube".')
-  parser.add_argument('--solver', default="./astar.py", help='executable that solves the algorithm')
-  parser.add_argument('--game', default=None, help='Game ID')
-  parser.add_argument('--size', nargs=2, type=int, default=(4,4), help='Board size')
-  args = parser.parse_args()
-
-  main(game_id=args.game, solver=args.solver, size=args.size)
+    parseargs()
