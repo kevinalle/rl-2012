b4d5bd10b1be750b7d7619599cb216fbd3bb4f1f
marianosemelman@gmail.com <marianosemelman@gmail.com@5eba630a-98d1-509a-3ca0-3178cf03187c>
2012-07-11 23:31:37 +0000

    comete esta puto

--- cube-solver/src/cubesolver/cube.py
+++ cube-solver/src/cubesolver/cube.py
@@ -1,38 +1,14 @@
 #!/usr/bin/env python
 """Correr el cube y mandarle acciones"""
 
-import commands, os, time, random, re, argparse
+import commands, os, time, random, argparse
+from util import decode, draw
 
 def move(where):
     """Espera .1s y apreta la tecla "where" (en la ventana activa)"""
     time.sleep(.1)
-    os.system("xdotool key %s"%where)
+    os.system("xdotool key %s" % where)
 
-def decode(gameid):
-    """
-    Extrae las condiciones iniciales del tablero a partir de un gameID
-    
-    El gameID es un string tipo "c4x4:62A4,11", donde "c" significa que es un
-    cubo, "4x4" es el tamanio del tablero, "62A4" es la representacion hexa
-    del color de las celdas del tablero y "11" es la posicion inicial del cubo.
-    """
-    if not re.match("c\d{1,2}x\d{1,2}:[A-F0-9]+,\d+", gameid):
-        print "Bad GameID"
-        exit(1)
-    width, height = [int(val) for val in gameid[1:].split(":")[0].split("x")]
-    boardid, pos = gameid.split(":")[1].split(",")
-    # construir una lista a partir de la representacin bin del tablero inicial
-    # (pasar el hexadecimal a binario, sacarle el "0b" del principio, volver a
-    # agregarle los 0s de la izquierda que corresponden, y truncarlo al tamanio
-    # del tablero (si no tenia multiplo de 4 celdas))
-    boardb = (bin(int(boardid, 16))[2:]).zfill(4*len(boardid))[:width * height]
-    # representacion en ASCII del tablero: _ celda blanca, # celda azul, C cubo
-    board = [{"0":"_", "1": "#"}[b] for b in boardb]
-    board[int(pos)] = "C"
-    # agregar "\n" cada w celdas
-    board_str = '\n'.join(
-        [''.join(board[i * width:(i + 1) * width]) for i in range(height)])
-    return {"w": width, "h": height, "board": board_str, "pos": pos}
 
 def main(game_id=None, solver=None, size=(4, 4)):
     """Correr cube, correr solver, mandarle las acciones del solver"""
@@ -43,12 +19,12 @@ def main(game_id=None, solver=None, size=(4, 4)):
     print gameid
     # parsear el id del juego y extraer el tablero inicial y posicion del cubo.
     game = decode(gameid)
-    print game["board"]
+    draw(*game)
 
     if not game_id:
         # correr el cube con la instancia que generamos antes (abre una ventana
         # con el mismo juego que generamos y decodificamos antes.
-        os.system("cube %s &"%gameid)
+        os.system("cube %s &" % gameid)
         # tomar el tiempo
         starttime = time.time()
 
@@ -63,11 +39,11 @@ def main(game_id=None, solver=None, size=(4, 4)):
     windowid = commands.getoutput(
                    'xwininfo -name "Cube" | grep "Window id" | cut -d\    -f4')
     # activar esa ventana para poder mandarle teclas
-    os.system("xdotool windowactivate %s"%windowid)
-    
+    os.system("xdotool windowactivate %s" % windowid)
+
     if not game_id:
         # (darle 2s para que se abra)
-        tardo = time.time()-starttime
+        tardo = time.time() - starttime
         if solver:
             print "solver took %ss" % tardo
         if tardo < 2:
