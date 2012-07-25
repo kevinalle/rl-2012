ca538c955e914cd8fe875c8324dc19f62de29d84
kevinalle <kevinalle@5eba630a-98d1-509a-3ca0-3178cf03187c>
2012-07-25 21:42:20 +0000

    anda qlearning

--- trunk/cube-solver/src/cubesolver/vi.py
+++ trunk/cube-solver/src/cubesolver/vi.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-"""Solve Cube with A*"""
+"""Solve Cube with Value Iteration"""
 
 import numpy as np
 
-def vi(state, num_states, num_actions):
+def vi(T, R, num_states, num_actions):
     epsilon = 0.001
+    gamma = 0.9
 
     # Inicializar V y Q a valores arbitrarios
 
     Q = np.zeros((num_states, num_actions))
     V = np.zeros((num_states, 1))
-    stop = False
-    gamma = 0.9
 
     # Ciclar hasta que entre una iteracion y otra haya poco (<epsilon) cambio
     # en los valores
+    stop = False
     while not stop:
         delta = 0
         # Para cada estado, para cada accion...
@@ -23,13 +23,14 @@ def vi(state, num_states, num_actions):
                 # Guardar Q actual para luego compararlo con el actualizado y
                 # medir cuanto cambio (para saber cuando parar el ciclo
                 # principal)
-                q = Q(s, a)
+                q = Q[s, a]
 
                 # Actualizar Q (se puede optimizar para el cubo)
-                Q[s, a] = state.R[s, a] + gamma * state.T[s, :, a] * V
+                TsaxV = # T[s, :, a] * V
+                Q[s, a] = R[s, a] + gamma * TsaxV 
 
                 # Medir el cambio maximo en Q
-                delta = max(delta, abs(q - Q(s, a)))
+                delta = max(delta, abs(q - Q[s, a]))
 
                 # Calcular V como el Q maximo para cada estado
                 V = max(Q.T).T
@@ -39,3 +40,7 @@ def vi(state, num_states, num_actions):
 
     # Extraer el policy de Q
     return Q.max(1)
+
+if __name__ == "__main__":
+    Q = np.zeros((num_states, num_actions))
+    vi()
